b26f4d96c3d527752cdabb297db074ea8742cc4e
Aftaab Zaman <54945683+aftaabzaman@users.noreply.github.com>
2023-10-15 16:07:58 +0600

    Update reverse_integer_and_palindrome_simplified.py

--- reverse_integer_and_palindrome_simplified.py
+++ reverse_integer_and_palindrome_simplified.py
@@ -1,32 +1,35 @@
-# Let's say we have the number 1234, and we want to reverse it, which means we want to make it 4321. Here's how we can do it step by step:
+""" Let's say we have the number 1234, and we want to reverse it, which means we want to make it 4321. Here's how we can do it step by step:
 
-# Start with an empty result: We'll start with an empty result, which is just a number with no digits. Let's call it reversed_num, and for now, it's 0.
+Start with an empty result: We'll start with an empty result, which is just a number with no digits. Let's call it reversed_num, and for now, it's 0.
 
-# Take the last digit of the original number: We start with the rightmost digit of the original number, which is 4 in our example.
+Take the last digit of the original number: We start with the rightmost digit of the original number, which is 4 in our example.
 
-# Add the last digit to the reversed number: We take the 4 from the original number and add it to the right side of our reversed_num. So now, reversed_num is 4.
+Add the last digit to the reversed number: We take the 4 from the original number and add it to the right side of our reversed_num. So now, reversed_num is 4.
 
-# Remove the last digit from the original number: Now, we remove the 4 from the original number, and it becomes 123.
+Remove the last digit from the original number: Now, we remove the 4 from the original number, and it becomes 123.
 
-# Repeat the process: We go back to step 2 and repeat the process. We take the last digit of the new original number, which is 3.
+Repeat the process: We go back to step 2 and repeat the process. We take the last digit of the new original number, which is 3.
 
-# Add it to the reversed number: We add 3 to the right side of our reversed_num, so it becomes 43.
+Add it to the reversed number: We add 3 to the right side of our reversed_num, so it becomes 43.
 
-# Remove the last digit from the original number: We remove the 3 from the original number, and it becomes 12.
+Remove the last digit from the original number: We remove the 3 from the original number, and it becomes 12.
 
-# Repeat again: We go back to step 2 again and take the last digit, which is 2.
+Repeat again: We go back to step 2 again and take the last digit, which is 2.
 
-# Add it to the reversed number: We add 2 to the right side of our reversed_num, so it becomes 432.
+Add it to the reversed number: We add 2 to the right side of our reversed_num, so it becomes 432.
 
-# Remove the last digit from the original number: We remove the 2 from the original number, and it becomes 1.
+Remove the last digit from the original number: We remove the 2 from the original number, and it becomes 1.
 
-# Repeat one last time: We take the last digit, which is 1.
+Repeat one last time: We take the last digit, which is 1.
 
-# Add it to the reversed number: We add 1 to the right side of our reversed_num, so it becomes 4321.
+Add it to the reversed number: We add 1 to the right side of our reversed_num, so it becomes 4321.
 
-# Remove the last digit from the original number: We remove the 1 from the original number, and it becomes 0.
+Remove the last digit from the original number: We remove the 1 from the original number, and it becomes 0.
 
-# Now, the original number is empty, and our reversed_num is 4321, which is the reverse of the original number, 1234. We're done!
+Now, the original number is empty, and our reversed_num is 4321, which is the reverse of the original number, 1234. We're done!
+
+Just check if the reversed version is equal to the user provided given number, DONE!
+"""
 
 
 
